67c79c218acf22720f3734a0e9907e77866d88cb
Shrena Patel <shrena.p@hotmail.co.uk>
2022-06-02 14:45:32 +1200

    dress the clown done up to step 11

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,34 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+let headIndex = 0
+
+let clothingIndex = 0
+// for clothingIndex: 0 = head, 1 = body, 2 = shoes
+
+function changeClownHead(moveNumber) {
+  let headImage = document.getElementById('head')
+
+  headIndex += moveNumber
+  if (headIndex < 0) {
+    headIndex = 5
+  }
+
+  if(headIndex > 5) {
+    headIndex = 0
+  }
+  let headImageSrc = "./images/head" + headIndex + ".png" 
+  headImage.src = headImageSrc
+}
+
+window.addEventListener('keydown', function(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      changeClownHead(-1)
+      break
+    case 'ArrowRight':
+      changeClownHead(1)
+      break
+  }
+})
